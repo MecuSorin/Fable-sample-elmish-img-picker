0bf96aa800c9677cb8c017acdd0476aea6b237b5
Mecu Sorin <mecusorin@gmail.com>
2017-06-11 21:30:10 +0300

    A way to reset it

--- src/app.fs
+++ src/app.fs
@@ -10,6 +10,7 @@ open Fable.Core
 open Fable.Import
 open Elmish
 type ImageURL = string
+
 // MODEL
 
 type Model = 
@@ -23,7 +24,18 @@ type Msg =
     | LoadImages
     | LoadedImages of ImageURL[]
     | PickedImage of ImageURL
-let init() : Model = Iddle
+    | ErrorRaised of exn
+let init() = Iddle, Cmd.none
+
+module IOLogic =
+    let cats () = [|
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJB1DCcOWYml3Mni8ra2yppq-S0nrhG7Y0Zn7VMVzaC3Sw-W1Z"
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGsNE90XVjKjGeVwu76dCyft44wphkhQJ6nJ7FMr9l2mCLB6Px_g"
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiiu3AGeFA-5Jb6UVWIKQ5EfIvtI9Lp7QHV8loDP4YvBrVrJneEA"
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT99ACj8dZ91EXXS-bslXJg9gPv3lRzoqPTa1vv8qGcNi0shtNVhg"
+    |]
+    let getImages () =
+        Cmd.ofFunc cats () LoadedImages ErrorRaised
 
 // UPDATE
 
@@ -32,20 +44,12 @@ let update (msg:Msg) (model:Model) =
     | PickedImage url ->
         match model with
         | Images images -> 
-            ImagePicked (url, images)
-        | ImagePicked (_, images) -> ImagePicked (url, images)
-        | _ -> Error "Invalid state: Image was picked but i have no images list"
-    | LoadedImages images ->
-        let cats = [|
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJB1DCcOWYml3Mni8ra2yppq-S0nrhG7Y0Zn7VMVzaC3Sw-W1Z"
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGsNE90XVjKjGeVwu76dCyft44wphkhQJ6nJ7FMr9l2mCLB6Px_g"
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiiu3AGeFA-5Jb6UVWIKQ5EfIvtI9Lp7QHV8loDP4YvBrVrJneEA"
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT99ACj8dZ91EXXS-bslXJg9gPv3lRzoqPTa1vv8qGcNi0shtNVhg"
-        |]
-    
-        Images images
-    | LoadImages -> LoadingImages
-
+            ImagePicked (url, images), Cmd.none
+        | ImagePicked (_, images) -> ImagePicked (url, images), Cmd.none
+        | _ -> Error "Invalid state: Image was picked but i have no images list", Cmd.none          // should return model, Cmd.from messsage something with exn
+    | LoadedImages images -> Images images, Cmd.none
+    | LoadImages -> LoadingImages, (IOLogic.getImages ())
+    | ErrorRaised e -> Error (e.ToString()), Cmd.none
 
 module View =
     open Fable.Core.JsInterop
@@ -61,11 +65,11 @@ module View =
     let images signal imagesArray =
         imagesArray
         |> Array.map (fun image ->
-            R.a [ OnClick <| signal (PickedImage image)] [ R.image [ Src image; Width (U2.Case1 200.)] []])
+            R.a [ OnClick <| signal (PickedImage image)] [ R.img [ Src image ] ])
         |> Array.toList
         |> R.div []
-    
-    let imagePicked pickedImage images =
+        
+    let imagePicked pickedImage images signal =
         R.div[] [
             images
             |> Array.toList
@@ -74,7 +78,9 @@ module View =
                             (if image = pickedImage then "selected " else "") |> R.str
                             R.str image])
             |> R.ul []
-            R.image [Src pickedImage][]
+            R.img [Src pickedImage]
+
+            R.button [OnClick <| signal (LoadedImages images)] [ R.str "Pick other image" ]
         ]
     
     let error message =
@@ -88,7 +94,7 @@ let view model dispatch =
     | Iddle -> View.iddle signal
     | LoadingImages -> View.loadingImages ()
     | Images images -> View.images signal images
-    | ImagePicked (pickedImage, images) -> View.imagePicked pickedImage images
+    | ImagePicked (pickedImage, images) -> View.imagePicked pickedImage images signal
     | Error message -> View.error message
     |> List.singleton
     |> Fable.Helpers.React.div []
@@ -96,7 +102,7 @@ let view model dispatch =
 open Elmish.React
 
 // App
-Program.mkSimple init update view
+Program.mkProgram init update view
 |> Program.withConsoleTrace
 |> Program.withReact "elmish-app"
 |> Program.run
