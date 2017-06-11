c46a3d580c7359b352ee6fdb6f6c4080d5b03b9f
Mecu Sorin <mecusorin@gmail.com>
2017-06-11 14:30:32 +0300

    No real loading of images

--- src/app.fs
+++ src/app.fs
@@ -1,3 +1,4 @@
+
 module App
 
 (**
@@ -8,36 +9,82 @@ module App
 open Fable.Core
 open Fable.Import
 open Elmish
-
+type ImageURL = string
 // MODEL
 
-type Model = int
+type Model = 
+    | Iddle
+    | LoadingImages
+    | Images of ImageURL[]
+    | ImagePicked of ImageURL * ImageURL[]
+    | Error of string
 
 type Msg =
-| Increment
-| Decrement
-
-let init() : Model = 0
+    | LoadImages
+    | LoadedImages of ImageURL[]
+    | PickedImage of ImageURL
+let init() : Model = Iddle
 
 // UPDATE
 
 let update (msg:Msg) (model:Model) =
     match msg with
-    | Increment -> model + 1
-    | Decrement -> model - 1
+    | PickedImage url ->
+        match model with
+        | Images images -> 
+            ImagePicked (url, images)
+        | ImagePicked (_, images) -> ImagePicked (url, images)
+        | _ -> Error "Invalid state: Image was picked but i have no images list"
+    | LoadedImages images ->
+        Images images
+    | LoadImages -> LoadingImages
 
-open Fable.Core.JsInterop
-open Fable.Helpers.React.Props
-module R = Fable.Helpers.React
+
+module View =
+    open Fable.Core.JsInterop
+    open Fable.Helpers.React.Props
+    module R = Fable.Helpers.React
+
+    let iddle signal =
+        R.button [ OnClick <| signal LoadImages] [ R.str "Load images" ]
+    
+    let loadingImages () =
+        R.str "Loading images. Please wait ..."
+
+    let images signal imagesArray =
+        imagesArray
+        |> Array.map (fun image ->
+            R.a [ OnClick <| signal (PickedImage image)] [ R.image [ Src image; Width (U2.Case1 200.)] []])
+        |> Array.toList
+        |> R.div []
+    
+    let imagePicked pickedImage images =
+        R.div[] [
+            images
+            |> Array.toList
+            |> List.map (fun image ->
+                R.li [] [ 
+                            (if image = pickedImage then "selected " else "") |> R.str
+                            R.str image])
+            |> R.ul []
+            R.image [Src pickedImage][]
+        ]
+    
+    let error message =
+        R.str message
 
 // VIEW (rendered with React)
-
 let view model dispatch =
-
-  R.div []
-      [ R.button [ OnClick (fun _ -> dispatch Decrement) ] [ R.str "-" ]
-        R.div [] [ R.str (sprintf "%A" model) ]
-        R.button [ OnClick (fun _ -> dispatch Increment) ] [ R.str "+" ] ]
+    let signal (msg: Msg) =
+        fun _ -> dispatch msg
+    match model with
+    | Iddle -> View.iddle signal
+    | LoadingImages -> View.loadingImages ()
+    | Images images -> View.images signal images
+    | ImagePicked (pickedImage, images) -> View.imagePicked pickedImage images
+    | Error message -> View.error message
+    |> List.singleton
+    |> Fable.Helpers.React.div []
 
 open Elmish.React
 
